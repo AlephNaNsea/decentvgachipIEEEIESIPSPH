5262761996d68bc911380a4e7a60e00bc7cf05ec
Chico <olaguer.chicoandre@gmail.com>
2026-05-01 19:14:32 +0800

    Replace tt_um_AlephNaNsea with user_project
    Updated module instantiation to use user_project.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_AlephNaNsea_decentvgachipIEEEIESIPSPH (
+  tt_um_AlephNaNsea user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
